9d1f9366d824a5a80ef0a7a950abdbbac10448ae
Sebastian Heunke <sebastian.heunke@hm.edu>
2017-04-12 14:27:05 +0200

    Blank files

---
+++ src/main/java/edu/hm/fachklassen/Copy.java
@@ -0,0 +1,12 @@
+package edu.hm.fachklassen;
+
+/*
+*ShareIt
+* Date: 12.04.2017
+* java version 1.8.0_73
+* Windows 10 (1607)
+* Intel(R) Core(TM) i5-4200U CPU @ 1.60GHz 2.30 GHz
+* @Author Sebastian Heunke, heunke@hm.edu
+*/
+public class Copy {
+}
