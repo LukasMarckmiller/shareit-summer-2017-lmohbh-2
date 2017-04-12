159ff4231f859d17dba2b14d6935d4dd40d09846
Sebastian Heunke <sebastian.heunke@hm.edu>
2017-04-12 15:04:22 +0200

    Fachklassen implementation

--- src/main/java/edu/hm/fachklassen/Book.java
+++ src/main/java/edu/hm/fachklassen/Book.java
@@ -8,5 +8,58 @@ package edu.hm.fachklassen;
 * Intel(R) Core(TM) i5-4200U CPU @ 1.60GHz 2.30 GHz
 * @Author Sebastian Heunke, heunke@hm.edu
 */
-public class Book {
+public class Book extends Medium {
+
+    private final String author;
+
+    private final String isbn;
+    private Book() {
+        this("", "", "");
+    }
+
+    public Book(String titel, String author, String isbn) {
+        super(titel);
+        if (author == null || isbn == null)
+            throw new IllegalArgumentException("Invalid author or isbn!");
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString(){
+        return "[" + getAuthor() + ": " + getTitel() + " (" + getIsbn() + ")]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Book book = (Book) o;
+
+        if (!super.equals(book))
+            return false;
+        if (!getAuthor().equals(book.getAuthor()))
+            return false;
+        return getIsbn().equals(book.getIsbn());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + getAuthor().hashCode();
+        result = 31 * result + getIsbn().hashCode();
+        return result;
+    }
 }
