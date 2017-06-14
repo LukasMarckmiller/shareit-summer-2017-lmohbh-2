8d55bd34a45524c5b04dd43e505fbc38210a5696
Sebastian Heunke <sebastian.heunke@hm.edu>
2017-06-14 14:01:35 +0200

    web xml

--- src/main/java/edu/hm/ShareItApplication.java
+++ src/main/java/edu/hm/ShareItApplication.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
+import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 
 /*
 *ShareIt
@@ -18,6 +19,11 @@ public class ShareItApplication extends ResourceConfig {
     @Inject
     public ShareItApplication(ServiceLocator serviceLocator){
         GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
-        GuiceIntoHK2BRidge guiceBridge =
+        GuiceIntoHK2Bridge guiceBridge
+                = serviceLocator.getService(GuiceIntoHK2Bridge.class);
+        guiceBridge.bridgeGuiceInjector(
+                ShareitServletContextListener.getInjectorInstance());
+
+
     }
 }
