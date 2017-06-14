0cc0df57cc71f29529abcb36b4c7b41d2d05ad5a
Sebastian Heunke <sebastian.heunke@hm.edu>
2017-06-14 14:29:52 +0200

    Corrected wrong imports.

--- src/main/java/edu/hm/ShareItApplication.java
+++ src/main/java/edu/hm/ShareItApplication.java
@@ -1,29 +1,23 @@
 package edu.hm;
 
-import com.google.inject.Inject;
+import javax.inject.Inject;
+
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 
-/*
-*ShareIt
-* Date: 14.06.2017
-* java version 1.8.0_73
-* Windows 10 (1607)
-* Intel(R) Core(TM) i5-4200U CPU @ 1.60GHz 2.30 GHz
-* @Author Sebastian Heunke, heunke@hm.edu
-*/
+/**
+ * Application class to enable guice within jersey.
+ * @author <a mailto:axel.boettcher@hm.edu>Axel B&ouml;ttcher</a>
+ */
 public class ShareItApplication extends ResourceConfig {
 
     @Inject
-    public ShareItApplication(ServiceLocator serviceLocator){
+    public ShareItApplication(ServiceLocator serviceLocator) {
         GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
-        GuiceIntoHK2Bridge guiceBridge
-                = serviceLocator.getService(GuiceIntoHK2Bridge.class);
-        guiceBridge.bridgeGuiceInjector(
-                ShareitServletContextListener.getInjectorInstance());
-
-
+        GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
+        guiceBridge.bridgeGuiceInjector(ShareitServletContextListener.getInjectorInstance());
     }
+
 }
--- src/main/java/edu/hm/ShareitServletContextListener.java
+++ src/main/java/edu/hm/ShareitServletContextListener.java
@@ -1,18 +1,10 @@
-
-/*
-*ShareIt
-* Date: 14.06.2017
-* java version 1.8.0_73
-* Windows 10 (1607)
-* Intel(R) Core(TM) i5-4200U CPU @ 1.60GHz 2.30 GHz
-* @Author Sebastian Heunke, heunke@hm.edu
-*/
 package edu.hm;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
+
 import edu.hm.shareit.resources.BookService;
 import edu.hm.shareit.resources.BookServiceImpl;
 import edu.hm.shareit.resources.DiscService;
@@ -20,11 +12,12 @@ import edu.hm.shareit.resources.DiscServiceImpl;
 
 /**
  * Context Listener to enable usage of google guice together with jersey.
+ * @author <a mailto:axel.boettcher@hm.edu>Axel B&ouml;ttcher</a>
+ *
  */
 public class ShareitServletContextListener extends GuiceServletContextListener {
 
-    private static final Injector injector
-            = Guice.createInjector(new ServletModule() {
+    private static final Injector injector = Guice.createInjector(new ServletModule() {
         @Override
         protected void configureServlets() {
             bind(BookService.class).to(BookServiceImpl.class);
@@ -38,12 +31,11 @@ public class ShareitServletContextListener extends GuiceServletContextListener {
     }
 
     /**
-     * 30 * This method is only required for the HK2-Guice-Bridge in the
-     * Application class.
-     *
+     * This method is only required for the HK2-Guice-Bridge in the Application class.
      * @return Injector instance.
      */
     static Injector getInjectorInstance() {
         return injector;
     }
+
 }
